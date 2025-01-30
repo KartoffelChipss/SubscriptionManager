76f41582161baac5df3ba583f9ef603aefe17531
KartoffelChips <jan@strassburger.org>
2025-01-30 17:23:20 +0100

    Recovered 5129e6d33e7ffdeb02d7fd2b215472083b1ff692

---
+++ src/main/java/org/strassburger/subscriptionmanager/presenter/DeleteSubscriptionPresenter.java
@@ -0,0 +1,48 @@
+package org.strassburger.subscriptionmanager.presenter;
+
+import org.strassburger.subscriptionmanager.model.DatabaseManager;
+import org.strassburger.subscriptionmanager.model.entity.Subscription;
+import org.strassburger.subscriptionmanager.view.DeleteSubscriptionView;
+import org.strassburger.tui4j.formatting.TextFormatter;
+import org.strassburger.tui4j.input.validationrules.ValidationRule;
+
+public class DeleteSubscriptionPresenter {
+    private final DeleteSubscriptionView view;
+    private final DatabaseManager dbManager;
+
+    public DeleteSubscriptionPresenter(DeleteSubscriptionView view, DatabaseManager dbManager) {
+        this.view = view;
+        this.dbManager = dbManager;
+    }
+
+
+    public void start() {
+        view.sendStartMessage();
+
+        String name = view.readName(getNameDoesNotExistValidationRule(dbManager));
+        Subscription subscription = dbManager.getSubscriptionRepository().getSubscriptionByName(name).get();
+        dbManager.getSubscriptionRepository().deleteSubscription(subscription.getId());
+
+        view.sendSubscriptionDeleteSuccessMessage();
+        view.enterToContinue();
+    }
+
+    /**
+     * Returns a validation rule that checks if the subscription with the given name does not exist.
+     * @param dbManager Database manager used to check if the subscription does not exist.
+     * @return The validation rule.
+     */
+    private ValidationRule<String> getNameDoesNotExistValidationRule(DatabaseManager dbManager) {
+        return new ValidationRule<String>() {
+            @Override
+            public boolean validate(String s) {
+                return dbManager.getSubscriptionRepository().getSubscriptionByName(s).isPresent();
+            }
+
+            @Override
+            public String getErrorMessage() {
+                return TextFormatter.format("&cSubscription with this name does not exist.");
+            }
+        };
+    }
+}
---
+++ src/main/java/org/strassburger/subscriptionmanager/view/DeleteSubscriptionView.java
@@ -0,0 +1,44 @@
+package org.strassburger.subscriptionmanager.view;
+
+import org.strassburger.tui4j.formatting.Printer;
+import org.strassburger.tui4j.input.ContinueInput;
+import org.strassburger.tui4j.input.TextInput;
+import org.strassburger.tui4j.input.validationrules.TextValidationRules;
+import org.strassburger.tui4j.input.validationrules.ValidationRule;
+
+public class DeleteSubscriptionView {
+    public void sendStartMessage() {
+        Printer.println("");
+        Printer.println("&a&nDelete Subscription");
+        //Printer.println("&7Please enter the following information:");
+        Printer.println("");
+    }
+
+    /**
+     * Reads the name of the subscription from the UI
+     * @param nameDoesNotExistValidationRule Validation rule to check if the name exists.
+     * @return The name of the subscription.
+     */
+    public String readName(ValidationRule<String> nameDoesNotExistValidationRule) {
+        return new TextInput()
+                .setLabel("Name: ")
+                .setRetryOnInvalid(true)
+                .setInline(true)
+                .addValidationRules(
+                        TextValidationRules.minLength(1),
+                        TextValidationRules.maxLength(255),
+                        nameDoesNotExistValidationRule
+                )
+                .read();
+    }
+
+    public void sendSubscriptionDeleteSuccessMessage() {
+        Printer.println("&aSubscription deleted successfully.");
+    }
+
+    public void enterToContinue() {
+        new ContinueInput()
+                .setLabel("Press ENTER to continue")
+                .read();
+    }
+}
