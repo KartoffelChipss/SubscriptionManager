a49e06642f4ebd719da2a7b39af47887d71e5e1e
KartoffelChips <jan@strassburger.org>
2025-02-06 22:33:46 +0100

    Fixed database file initialization

--- src/main/java/org/strassburger/subscriptionmanager/Main.java
+++ src/main/java/org/strassburger/subscriptionmanager/Main.java
@@ -4,6 +4,7 @@ import org.strassburger.subscriptionmanager.model.DatabaseManager;
 import org.strassburger.subscriptionmanager.model.SQLiteDatabaseManager;
 import org.strassburger.subscriptionmanager.presenter.WelcomeWindowPresenter;
 import org.strassburger.subscriptionmanager.view.WelcomeWindowView;
+import org.strassburger.tui4j.formatting.Printer;
 
 import java.util.logging.LogManager;
 
@@ -17,9 +18,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        DatabaseManager dbManager = new SQLiteDatabaseManager();
-        WelcomeWindowPresenter presenter = new WelcomeWindowPresenter(new WelcomeWindowView(), dbManager);
+        try {
+            DatabaseManager dbManager = new SQLiteDatabaseManager();
+            WelcomeWindowPresenter presenter = new WelcomeWindowPresenter(new WelcomeWindowView(), dbManager);
 
-        presenter.start();
+            presenter.start();
+        } catch (SQLiteDatabaseManager.DatabaseInitializationException e) {
+            Printer.println("&cFailed to initialize database: " + e.getMessage());
+            System.exit(1);
+        }
     }
 }
--- src/main/java/org/strassburger/subscriptionmanager/model/SQLiteDatabaseManager.java
+++ src/main/java/org/strassburger/subscriptionmanager/model/SQLiteDatabaseManager.java
@@ -4,26 +4,71 @@ import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLiteDatabaseManager extends DatabaseManager {
     private final DSLContext dslContext;
+    public static final String DB_FILE_NAME = "subscription_manager.db";
 
-    public SQLiteDatabaseManager() {
+    public SQLiteDatabaseManager() throws DatabaseInitializationException {
         try {
-            final String databaseUrl = "jdbc:sqlite:src/main/resources/subscription_manager.db";
+            File dbFile = createDatabaseFile();
+
+            final String databaseUrl = "jdbc:sqlite:" + dbFile.getAbsolutePath();
             Connection connection = DriverManager.getConnection(databaseUrl);
 
             this.dslContext = DSL.using(connection, SQLDialect.SQLITE);
         } catch (SQLException e) {
-            throw new RuntimeException("Failed to initialize SQLiteDatabaseManager", e);
+            throw new DatabaseInitializationException("Failed to initialize SQLiteDatabaseManager", e);
+        } catch (IOException e) {
+            throw new DatabaseInitializationException("Failed to create database file", e);
         }
+    }
+
+    /**
+     * Creates the database file if it does not exist.
+     * @return The database file
+     * @throws IOException If an I/O error occurs
+     */
+    private File createDatabaseFile() throws IOException {
+        File dbFile = new File(DB_FILE_NAME);
+
+        if (dbFile.exists()) {
+            return dbFile;
+        }
+
+        // Copy the database file from the resources to the current directory
+        try (InputStream dbInputStream = getClass().getClassLoader().getResourceAsStream(DB_FILE_NAME)) {
+            if (dbInputStream == null) {
+                throw new IOException("Database file not found: " + DB_FILE_NAME);
+            }
+
+            try (FileOutputStream outputStream = new FileOutputStream(dbFile)) {
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = dbInputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, length);
+                }
+            }
+        }
+
+        return dbFile;
     }
 
     @Override
     public DSLContext getDslContext() {
         return dslContext;
+    }
+
+    public static class DatabaseInitializationException extends Exception {
+        public DatabaseInitializationException(String message, Throwable cause) {
+            super(message, cause);
+        }
     }
 }
